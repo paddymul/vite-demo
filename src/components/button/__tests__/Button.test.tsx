1a9ebf20108a8135f562d7c807e4eaa1cee8aa90
Paddy Mullen <paddy@paddymullen.com>
2024-11-14 15:29:11 -0500

    build works locally

--- src/components/button/__tests__/Button.test.tsx
+++ src/components/button/__tests__/Button.test.tsx
@@ -1,5 +1,5 @@
 //Button/__test__/Button.test.tsx
-import React from "react";
+//import React from "react";
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Button from "../Button";
@@ -7,7 +7,8 @@ import Button from "../Button";
 describe("Button component", () => {
   it("Button should render correctly", () => {
     render(<Button />);
-    const button = screen.getByRole("button");
+      const button = screen.getByRole("button");
+      //@ts-ignore
     expect(button).toBeInTheDocument();
   });
 });
